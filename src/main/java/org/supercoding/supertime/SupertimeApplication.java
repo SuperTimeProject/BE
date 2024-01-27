06e24cb5db03ed6503af7619272984989cb08401
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-01-27 20:04:37 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/supercoding/supertime/SupertimeApplication.java
@@ -0,0 +1,13 @@
+package org.supercoding.supertime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupertimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupertimeApplication.class, args);
+    }
+
+}
