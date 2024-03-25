ce6ecd0d5bee109e6b0c4f72b2f4d1613dbf457f
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-03-25 16:41:15 +0900

    feat: 기본 관리자 계정 추가 로직 구현
    기본적으로 생성되는 관리자 계정에 대한 로직을 추가하였습니다.

---
+++ src/main/java/org/supercoding/supertime/golbal/web/entity/dataLoader/CreateDefaultAdmin.java
@@ -0,0 +1,56 @@
+package org.supercoding.supertime.golbal.web.entity.dataLoader;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.supercoding.supertime.golbal.web.enums.Part;
+import org.supercoding.supertime.golbal.web.enums.Roles;
+import org.supercoding.supertime.golbal.web.enums.Valified;
+import org.supercoding.supertime.user.repository.UserRepository;
+import org.supercoding.supertime.user.web.entity.user.UserEntity;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CreateDefaultAdmin implements CommandLineRunner {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        createDefaultAdmin();
+    }
+
+    @Value("${spring.default.admin_id}")
+    private String defaultUserId;
+    @Value("${spring.default.admin_pwd}")
+    private String defaultPassword;
+
+    private void createDefaultAdmin(){
+        final String defaultUserName = "관리자";
+        final String defaultUserNickName = "관리자";
+
+        if(!userRepository.existsByUserId(defaultUserId)){
+            UserEntity defaultUser = UserEntity.builder()
+                    .userId(defaultUserId)
+                    .userPassword(passwordEncoder.encode(defaultPassword))
+                    .userName(defaultUserName)
+                    .userNickname(defaultUserNickName)
+                    .roles(Roles.ROLE_ADMIN)
+                    .part(Part.PART_UNDEFINED)
+                    .isDeleted(0)
+                    .valified(Valified.COMPLETED)
+                    .build();
+
+            log.info("[CREATE_DEFAULT_ADMIN] 관리자 계정이 생성되었습니다.");
+
+            userRepository.save(defaultUser);
+        } else {
+            log.info("[CREATE_DEFAULT_ADMIN] 이미 관리자 계정이 존재하여 생성하지 않았습니다.");
+        }
+
+    }
+}
